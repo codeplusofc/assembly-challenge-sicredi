9ee014d2e985ca2660d617c9939b820c937b1871
Rafael Cabral <84587562+rafaelzi@users.noreply.github.com>
2025-01-06 22:16:18 -0300

    First commit

---
+++ src/test/java/com/votation/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
